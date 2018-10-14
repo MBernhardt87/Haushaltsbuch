4d452108f60d5da807eb7857aa8a89b947d5258b
Marcus <info@aurora-analytics.de>
2018-10-14 13:41:46 +0200

    Backend Logik eingefügt

--- app.R
+++ app.R
@@ -10,11 +10,12 @@ library(ggplot2)
 library(rhandsontable)
 
 ##Source Util Files
-
+source("./Utils/BackendLogic.R")
 
 ## Adjust Options
 options(scipen=10)
 options(shiny.launch.browser = TRUE)
 
+## Launch Shiny App
 shinyApp(ui = source("ui.R"), server = source("server.R"))
 
